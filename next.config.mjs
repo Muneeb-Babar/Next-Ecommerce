0eca3261173fddfcc0ec4deafb67c01df09cd332
Muneeb-Babar <muneeb63ahmad63@gmail.com>
2024-12-15 22:32:03 +0500

    fix bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    images: {
-      domains: ["example.com"], // Replace with your image domains
-    },
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ["@svgr/webpack"],
-      });
-      return config;
-    },
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
